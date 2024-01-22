6b64086ae6bfca6599b6072c3afa62ef60e7604e
Kwon Jong Seong <kjsdev@jobkorea.co.kr>
2024-01-22 17:13:28 +0900

    init front todoapp

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <button className="">+</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
